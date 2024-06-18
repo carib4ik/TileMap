ad07126778e88fcb90e6cce9836e74004c97b4e5
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-06-18 11:14:55 +0800

    tryed to add cursor екфслштп

--- Assets/Scripts/MapBuilder.cs
+++ Assets/Scripts/MapBuilder.cs
@@ -1,7 +1,12 @@
+using System;
 using UnityEngine;
 
 public class MapBuilder : MonoBehaviour
 {
+    [SerializeField] private Grid _grid;
+    
+    private GameObject _tile;
+    
     /// <summary>
     /// Данный метод вызывается автоматически при клике на кнопки с изображениями тайлов.
     /// В качестве параметра передается префаб тайла, изображенный на кнопке.
@@ -9,6 +14,23 @@ public class MapBuilder : MonoBehaviour
     /// </summary>
     public void StartPlacingTile(GameObject tilePrefab)
     {
+        _tile = Instantiate(tilePrefab);
+    }
+    
+    private void Update()
+    {
+        // var mouseScreenPosition = Input.mousePosition;
+        // mouseScreenPosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        // var mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
         
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        var cellPosition = _grid.WorldToCell(mousePosition);
+        var cellCenterPosition = _grid.GetCellCenterWorld(cellPosition);
+
+        if (_tile != null)
+        {
+            _tile.transform.position = cellCenterPosition;
+        }
     }
 }
